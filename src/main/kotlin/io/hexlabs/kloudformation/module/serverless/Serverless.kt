d77638b8b4c7ef4d11024464fea982ef6a5a490b
Chris Barbour <chris.barbour03@gmail.com>
2019-04-11 21:05:21 +0100

    added random uuid to role name and policy, fixed global vs individual roles

--- src/main/kotlin/io/hexlabs/kloudformation/module/serverless/Serverless.kt
+++ src/main/kotlin/io/hexlabs/kloudformation/module/serverless/Serverless.kt
@@ -13,30 +13,25 @@ import io.kloudformation.model.iam.PrincipalType
 import io.kloudformation.model.iam.action
 import io.kloudformation.model.iam.policyDocument
 import io.kloudformation.model.iam.resource
-import io.kloudformation.module.Modification
 import io.kloudformation.module.Module
 import io.kloudformation.module.ModuleBuilder
-import io.kloudformation.module.NoProps
 import io.kloudformation.module.OptionalModification
 import io.kloudformation.module.Properties
 import io.kloudformation.module.SubModules
 import io.kloudformation.module.builder
-import io.kloudformation.module.modification
 import io.kloudformation.module.optionalModification
 import io.kloudformation.property.aws.iam.role.policy
 import io.kloudformation.resource.aws.iam.Role
 import io.kloudformation.resource.aws.iam.role
-import io.kloudformation.resource.aws.s3.Bucket
-import io.kloudformation.resource.aws.s3.bucket
+import java.util.UUID
 
-class Serverless(val deploymentBucket: Bucket?, val globalRole: Role?, val functions: List<ServerlessFunction>) : Module {
+class Serverless(val globalRole: Role?, val functions: List<ServerlessFunction>) : Module {
 
     open class PrivateConfig(val securityGroups: Value<List<Value<String>>>? = null, val subnetIds: Value<List<Value<String>>>? = null)
     object NoPrivateConfig : PrivateConfig()
 
     class Parts(
-        val deploymentBucket: Modification<Bucket.Builder, Bucket, NoProps> = modification(),
-        val globalRole: OptionalModification<Role.Builder, Role, RoleProps> = optionalModification(absent = true)
+        val globalRole: OptionalModification<Role.Builder, Role, RoleProps> = optionalModification(absent = false)
     ) {
         val serverlessFunction = SubModules({ pre: ServerlessFunction.Predefined, props: ServerlessFunction.Props -> ServerlessFunction.Builder(pre, props) })
         fun serverlessFunction(
@@ -53,22 +48,16 @@ class Serverless(val deploymentBucket: Bucket?, val globalRole: Role?, val funct
     class Builder(
         val serviceName: String,
         val stage: String,
-        val bucket: Value<String>? = null,
+        val bucketName: Value<String>,
         val privateConfig: PrivateConfig? = null
     ) : ModuleBuilder<Serverless, Parts>(Parts()) {
 
         override fun KloudFormation.buildModule(): Parts.() -> Serverless = {
-            val bucketResource = if (bucket == null) deploymentBucket(NoProps) { props ->
-                bucket {
-                    modifyBuilder(props)
-                }
-            } else null
-            val bucketArn = bucketResource?.ref() ?: bucket!!
             val roleResource = roleFor(serviceName, stage, globalRole)
             val functions = serverlessFunction.modules().mapNotNull {
-                it.module(ServerlessFunction.Predefined(serviceName, stage, bucketArn, roleResource, privateConfig))()
+                it.module(ServerlessFunction.Predefined(serviceName, stage, bucketName, roleResource, privateConfig))()
             }
-            Serverless(bucketResource, roleResource, functions)
+            Serverless(roleResource, functions)
         }
 
         companion object {
@@ -83,7 +72,7 @@ class Serverless(val deploymentBucket: Bucket?, val globalRole: Role?, val funct
                     role(props.assumedRolePolicyDocument) {
                         policies(listOf(
                                 policy(
-                                        policyName = +"$stage-$serviceName-lambda",
+                                        policyName = +"$stage-$serviceName-${UUID.randomUUID()}",
                                         policyDocument = PolicyDocument(
                                                 version = IamPolicyVersion.V2.version,
                                                 statement = listOf(
@@ -100,7 +89,7 @@ class Serverless(val deploymentBucket: Bucket?, val globalRole: Role?, val funct
                                 )
                         ))
                         path("/")
-                        roleName(+"$serviceName-$stage-" + awsRegion + "-lambdaRole")
+                        roleName(+"$serviceName-$stage-" + awsRegion + "-${UUID.randomUUID()}")
                         managedPolicyArns(listOf(
                                 +"arn:" + awsPartition + ":iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
                         ))
@@ -115,7 +104,7 @@ class Serverless(val deploymentBucket: Bucket?, val globalRole: Role?, val funct
 fun KloudFormation.serverless(
     serviceName: String,
     stage: String = "dev",
-    bucketArn: Value<String>? = null,
+    bucketName: Value<String>,
     privateConfig: Serverless.PrivateConfig? = null,
     partBuilder: Serverless.Parts.() -> Unit = {}
-) = builder(Serverless.Builder(serviceName, stage, bucketArn, privateConfig), partBuilder)
+) = builder(Serverless.Builder(serviceName, stage, bucketName, privateConfig), partBuilder)
--- src/test/kotlin/io/hexlabs/kloudformation/module/serverless/ServerlessTest.kt
+++ src/test/kotlin/io/hexlabs/kloudformation/module/serverless/ServerlessTest.kt
@@ -1,6 +1,9 @@
 package io.hexlabs.kloudformation.module.serverless
 
 import io.kloudformation.model.KloudFormationTemplate
+import io.kloudformation.resource.aws.iam.Role
+import io.kloudformation.resource.aws.lambda.Function
+import io.kloudformation.resource.aws.logs.LogGroup
 import io.kloudformation.resource.aws.s3.bucket
 import io.kloudformation.toYaml
 import org.junit.jupiter.api.Test
@@ -8,109 +11,25 @@ import kotlin.test.expect
 
 class ServerlessTest {
 
+    inline fun <reified T> KloudFormationTemplate.filter() = resources.resources.toList().filter { it.second is T }
+
     @Test
-    fun `should have bucket bucket policy logGroup Role and Function by default`() {
+    fun `should have logGroup Role and Function by default`() {
         val template = KloudFormationTemplate.create {
             val myBucket = bucket("Bob") { bucketName("Bob") }
-            serverless("testService", bucketArn = myBucket.ref()) {
-                serverlessFunction(ServerlessFunction.Props(
-                        functionId = "myFunction",
-                        codeLocationKey = +"Dont know",
-                        handler = +"a.b.c",
-                        runtime = +"nodejs8.10")) {
-                }
-            } }.toYaml()
-        expect("""---
-AWSTemplateFormatVersion: "2010-09-09"
-Resources:
-  Bob:
-    Type: "AWS::S3::Bucket"
-    Properties:
-      BucketName: "Bob"
-  LogGroup:
-    Type: "AWS::Logs::LogGroup"
-    Properties:
-      LogGroupName: "/aws/lambda/testService-dev-myFunction"
-  Role:
-    Type: "AWS::IAM::Role"
-    Properties:
-      AssumeRolePolicyDocument:
-        Statement:
-        - Effect: "Allow"
-          Action:
-          - "sts:AssumeRole"
-          Principal:
-            Service:
-            - "lambda.amazonaws.com"
-        Version: "2012-10-17"
-      ManagedPolicyArns:
-      - Fn::Join:
-        - ""
-        - - "arn:"
-          - Ref: "AWS::Partition"
-          - ":iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-      Path: "/"
-      Policies:
-      - PolicyDocument:
-          Statement:
-          - Effect: "Allow"
-            Action:
-            - "logs:CreateLogStream"
-            Resource:
-            - Fn::Join:
-              - ""
-              - - "arn:"
-                - Ref: "AWS::Partition"
-                - ":logs:"
-                - Ref: "AWS::Region"
-                - ":"
-                - Ref: "AWS::AccountId"
-                - ":log-group:/aws/lambda/testService-dev-definition:*"
-          - Effect: "Allow"
-            Action:
-            - "logs:PutLogEvents"
-            Resource:
-            - Fn::Join:
-              - ""
-              - - "arn:"
-                - Ref: "AWS::Partition"
-                - ":logs:"
-                - Ref: "AWS::Region"
-                - ":"
-                - Ref: "AWS::AccountId"
-                - ":log-group:/aws/lambda/testService-dev-definition:*"
-                - ":*"
-          Version: "2012-10-17"
-        PolicyName: "dev-testService-lambda"
-      RoleName:
-        Fn::Join:
-        - ""
-        - - "testService-dev-"
-          - Ref: "AWS::Region"
-          - "-lambdaRole"
-  Function:
-    Type: "AWS::Lambda::Function"
-    DependsOn:
-    - "LogGroup"
-    - "Role"
-    Properties:
-      Code:
-        S3Bucket:
-          Ref: "Bob"
-        S3Key: "Dont know"
-      Handler: "a.b.c"
-      Role:
-        Fn::GetAtt:
-        - "Role"
-        - "Arn"
-      Runtime: "nodejs8.10"
-""") { template }
+            serverless("testService", bucketName = myBucket.ref()) {
+                serverlessFunction(functionId = "myFunction", codeLocationKey = +"Dont know", handler = +"a.b.c", runtime = +"nodejs8.10")
+            }
+        }
+        expect(1) { template.filter<Role>().size }
+        expect(1) { template.filter<LogGroup>().size }
+        expect(1) { template.filter<Function>().size }
     }
 
     @Test
     fun `should have bucket and bucket policy by default`() {
             val template = KloudFormationTemplate.create {
-                serverless("testService") {
+                serverless("testService", bucketName = +"bucket") {
                     serverlessFunction(
                         functionId = "myFunction",
                         codeLocationKey = +"Dont know",
@@ -131,5 +50,17 @@ Resources:
                 }
         }
         println(template.toYaml())
+    }
+
+    @Test
+    fun `should use global role for both functions`() {
+        val template = KloudFormationTemplate.create {
+            serverless("testService", bucketName = +"bucket") {
+                serverlessFunction(functionId = "myFunction", codeLocationKey = +"Dont know", handler = +"a.b.c", runtime = +"nodejs8.10")
+                serverlessFunction(functionId = "myFunction2", codeLocationKey = +"Dont know", handler = +"a.b.c", runtime = +"nodejs8.10")
+            }
+        }
+        val roles = template.resources.resources.toList().filter { (key, value) -> value.kloudResourceType == "AWS::IAM::Role" }
+        expect(1) { roles.size }
     }
 }
